6d75d3bac5e64fd9ab802aab59f2c062f797b4ea
shajoffcl <shaj.native@gmail.com>
2020-12-08 20:46:22 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
+const {newsArticleModel}=require('./connector');
 
 const onePageArticleCount = 10
 
@@ -8,6 +9,16 @@ const onePageArticleCount = 10
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.get('/newFeeds',(req,res)=>{
+    let limit=isNaN(req.query.limit) || !req.query.limit ?10:parseInt(req.query.limit);
+    let offset=isNaN(req.query.offset) || !req.query.offset ?0:parseInt(req.query.offset);
+
+    newsArticleModel.find().limit(limit).skip(offset).then((ele)=>{
+        res.send(ele);
+    });
+});
+
 
 
 
